ac49854b128322d475ba6ec7bdce51401c7eee49
Hyeongyu-lee <alruminumfire@gmail.com>
2020-05-28 10:36:01 -0400

    update_0528_2335

--- nodes/loadbalancer_node.cpp
+++ nodes/loadbalancer_node.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ros/ros.h>
+#include <pthread.h>
 #include <loadbalancer/loadbalancer.h>
 #include "block_device/LD_data.h"
 /*namespace Load_Balancer {
@@ -23,6 +24,7 @@ int main(int argc, char **argv)
   ros::Publisher LD2_pub = nh.advertise<block_device::LD_data>("LD2_msg",100);
   ros::Publisher LD3_pub = nh.advertise<block_device::LD_data>("LD3_msg",100);
   ros::Publisher LD4_pub = nh.advertise<block_device::LD_data>("LD4_msg",100);
+  ros::Publisher* pub_list[4]={&LD1_pub,&LD2_pub,&LD3_pub,&LD4_pub};
   block_device::LD_data msg;
   ros::Rate loop_rate(10);
   int count=0;
@@ -34,23 +36,9 @@ int main(int argc, char **argv)
     msg.data=count;
 
     ROS_INFO("%d :: send msg = %d (%d.%d)",msg.id, msg.data, msg.stamp.sec, msg.stamp.nsec);
-    switch(msg.id) {
-    case 1:
-      LD1_pub.publish(msg);
-      break;
-    case 2:
-      LD2_pub.publish(msg);
-      break;
-    case 3:
-      LD3_pub.publish(msg);
-      break;
-    case 4:
-      LD4_pub.publish(msg);
-      break;
-    default:
-      ROS_INFO("ERROR");
-      break;
-    }
+//    pthread_create();
+    pub_list[msg.id]->publish(msg);
+//    pthread_join();
 
     loop_rate.sleep();
 
