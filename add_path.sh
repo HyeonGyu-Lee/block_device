b5fc7cf31b2ec16fbcf4b4e4f76b25906c2322d2
Hyeongyu-lee <alruminumfire@gmail.com>
2020-06-01 02:00:46 -0400

    updata_060115

---
+++ add_path.sh
@@ -0,0 +1,3 @@
+sudo ln -s /usr/local/cuda/lib64/libcudart.so /usr/lib/
+sudo ln -s /usr/local/cuda/lib64/libcublas.so /usr/lib/
+sudo ln -s /usr/local/cuda/lib64/libcurand.so /usr/lib/
--- nodes/loadbalancer_node.cpp
+++ nodes/loadbalancer_node.cpp
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ros/ros.h>
+#include <vector>
 #include <pthread.h>
 #include <loadbalancer/loadbalancer.h>
 #include "block_device/LD_data.h"
+
+using namespace std;
 /*namespace Load_Balancer {
 
 class LoadControlNode
@@ -24,25 +27,29 @@ int main(int argc, char **argv)
   ros::Publisher LD2_pub = nh.advertise<block_device::LD_data>("LD2_msg",100);
   ros::Publisher LD3_pub = nh.advertise<block_device::LD_data>("LD3_msg",100);
   ros::Publisher LD4_pub = nh.advertise<block_device::LD_data>("LD4_msg",100);
-  ros::Publisher* pub_list[4]={&LD1_pub,&LD2_pub,&LD3_pub,&LD4_pub};
+  vector<ros::Publisher> pub_list;
+  pub_list.push_back(LD1_pub);
+  pub_list.push_back(LD2_pub);
+  pub_list.push_back(LD3_pub);
+  pub_list.push_back(LD4_pub);
+  
   block_device::LD_data msg;
   ros::Rate loop_rate(10);
-  int count=0;
+  int cnt=0;
 
   while(ros::ok())
   {
-    msg.id=count%4+1;
+    msg.id=cnt%4;
     msg.stamp=ros::Time::now();
-    msg.data=count;
+    msg.data=cnt;
 
-    ROS_INFO("%d :: send msg = %d (%d.%d)",msg.id, msg.data, msg.stamp.sec, msg.stamp.nsec);
+    ROS_INFO("LD%d :: send msg = %d (%d.%d)",msg.id+1, msg.data, msg.stamp.sec, msg.stamp.nsec);
 //    pthread_create();
-    pub_list[msg.id]->publish(msg);
+    pub_list.at(msg.id).publish(msg);
 //    pthread_join();
 
     loop_rate.sleep();
-
-    ++count;
+    cnt++;
   }
   
   return EXIT_SUCCESS;
